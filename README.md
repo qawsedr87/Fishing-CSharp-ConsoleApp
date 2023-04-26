55cca962a916f1dc505e2916051a6287bd9e3b7f
qawsedr87 <ryryryrylin@gmail.com>
2023-04-26 12:48:34 -0700

    feat: init

---
+++ README.md
@@ -0,0 +1,96 @@
+# Fishing Game 
+
+This repository is for the midterm project in C#II Spring 2023 Course. 
+
+1. Please check out `init` branch for the initialization input.
+> Note: You'll have to change the code in `init` branch, so it is not working `fish_final.cs`.
+
+2. Please check out `main` branch if you are instered in the implementation.
+
+Here is more details about this fishing game project, 
+
+## Features
+
+1. Players 
+    - Start with $0 cash.
+    - Makes money selling fish. Players then use the money to buy better fishing rods to catch better fish.
+    - Start with bamboo stick fishing rod. Costs $0 with +0 buff
+2. Sell fish
+    - Automatically sells all of user's fish in container. 
+    - Prices based on standard market price assigned to each fish (set inside Fish's subclass).
+    - If container is empty, tell player to come back when you have fish.
+3. Bass pro shop
+    - Print how much money players have at the top.
+    - Print the rods available on sale.
+    - Verify player has enough money to purchase rod.
+    - Allow player to buy rod if sufficient money.
+    - Fishing rods add buffs to help them catch more fish. 
+
+> Example: Suppose there's 30% chance of catching catfish. If I have a rod with +10 buff, that means there'll be 40% chance of catching catfish. Buff works on all fishes.
+
+4. Go fishing
+    - Ask users what lake they want to go to.
+    - Depending on the lake they go to, there will be different fishes and probabilities.
+    - Print all fish caught in the container. 
+5. Fish marketplace value
+    - Catfish: $10
+    - Bluefin: $500
+    - Albacore: $50
+    - Yellowtail: $100
+
+ 
+## How it works
+1. Lake class has these parameters: 
+    - Name
+    - List of fish available in the lake and their probabilities.
+2. Fish class has these parameters:
+    - Probably of being caught
+3. FishingRod class has these parameters:
+    - Name
+    - Price
+    - Buff
+ 
+
+```CSHARP
+// list of lakes
+List<Lake> all_lakes = new List<Lake>()
+{
+    // each lake takes in a list of fish that's available in the lake
+    new Lake("Lake Tahoe", new List<Fish>()
+    {
+        // each fish takes in the probability they bite the bait. So catfish has 35% chance of biting
+        new Catfish(35),
+
+        // albacore has 20% chance of biting
+        new Albacore(20),
+
+        // bluefin has 5% chance of biting
+        new Bluefin(5)
+
+    }),
+    // each lake takes in a list of fish that's available in the lake
+    new Lake("Big Bear lake", new List<Fish>()
+    {
+        // catfish less prolific in big bear, only 30% chance of biting.
+        new Catfish(30),
+
+        // albacore has 35% chance of biting
+        new Albacore(35),
+
+        // yellowtail has 15% chance of biting
+        new YellowTail(15),
+
+    })
+};
+
+// list of all fishing rods available at the store
+List<FishingRod> all_rods = new List<FishingRod>()
+{
+    // each fishing rod passes in the name, price, and buff added to probability.
+    new FishingRod("Swift Model E", 50, 2),
+    new FishingRod("Perry Water Eye Gouger", 200, 6),
+    new FishingRod("The Senator", 400, 10),
+    new FishingRod("Ocean Depleter", 1000, 20),
+
+};
+```
---
+++ fish_final.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace fishing_game
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            /* KEEP THIS START */
+
+            // list of lakes
+            List<Lake> all_lakes = new List<Lake>()
+            {
+                // each lake takes in a list of fish that's available in the lake
+                new Lake("Lake Tahoe", new List<Fish>()
+                {
+                    // each fish takes in the probability they bite the bait. So catfish has 35% chance of biting
+                    new Catfish(35),
+
+                    // albacore has 20% chance of biting
+                    new Albacore(20),
+
+                    // bluefin has 5% chance of biting
+                    new Bluefin(5)
+
+                }),
+                // each lake takes in a list of fish that's available in the lake
+                new Lake("Big Bear lake", new List<Fish>()
+                {
+                    // catfish less prolific in big bear, only 30% chance of biting.
+                    new Catfish(30),
+
+                    // albacore has 35% chance of biting
+                    new Albacore(35),
+
+                    // yellowtail has 15% chance of biting
+                    new YellowTail(15),
+
+                })
+            };
+
+            // list of all fishing rods available at the store
+            List<FishingRod> all_rods = new List<FishingRod>()
+            {
+                // each fishing rod passes in the name, price, and buff added to probability.
+                new FishingRod("Swift Model E", 50, 2),
+                new FishingRod("Perry Water Eye Gouger", 200, 6),
+                new FishingRod("The Senator", 400, 10),
+                new FishingRod("Ocean Depleter", 1000, 20),
+
+            };
+            /* KEEP THIS END */
+
+
+            Console.WriteLine("Welcome to deep sea fishing!");
+            Console.WriteLine("---------");
+
+            Console.WriteLine("1. Go fishing");
+            Console.WriteLine("2. Bass pro shop");
+            Console.WriteLine("3. Sell fish");
+            Console.WriteLine("What would you like to do?");
+
+            string input = Console.ReadLine();
+
+        }
+
+
+
+        private static void go_fishing(List<Fish> availableFishes)
+        {
+            int full_container = 4;
+            Random rand = new Random();
+
+            // list of fishes in our container
+
+
+            Console.WriteLine(string.Format("Our container holds {0} fish. Keep the ones you love.", full_container));
+            Console.WriteLine("Let's fish!");
+
+            // while we have space in our container...
+            while (container.Count < full_container)
+            {
+
+                // a random fish comes near our bait
+                int random_fish_index = rand.Next(availableFishes.Count);
+
+                // generate random number to see if fish bites bait
+                int fish_bites_chance = rand.Next(100);
+
+                // random chance if fish decides to bite the bait
+                if (availableFishes[random_fish_index].BitesBait(fish_bites_chance))
+                {
+
+                    // Fishing techniques
+                    Console.WriteLine("\nA fish bit your bait! What do you want to do?");
+                    Console.WriteLine("1. Reel in fish");
+                    Console.WriteLine("2. Use net\n");
+
+                    string input = Console.ReadLine();
+
+                    // if the user properly caught the fish...
+                    if (availableFishes[random_fish_index].IsCaught(input))
+                    {
+                        Console.WriteLine("\nGot it! Let's see what we caught.");
+
+                        // print the name of the fish
+                        Console.WriteLine(availableFishes[random_fish_index].Name);
+                        Console.WriteLine("");
+
+                        // add fish to container
+                        container.Add(availableFishes[random_fish_index]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oh no! The fish got away. Keep trying!\n\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("One minute went by...");
+                    Thread.Sleep(500);
+                }
+            }
+
+            Console.WriteLine("We're full! Let's look at what we caught.");
+
+            // print all fish
+
+
+            Console.WriteLine("\n\n\n");
+        }
+    }
+
+
+
+    /// <summary>
+    /// Base fish class
+    /// </summary>
+    class Fish
+    {
+
+        public string Name { get; set; }
+        public int MarketPrice { get; set; }
+        public int BiteProbability { get; set; }
+
+        public Fish(int probability)
+        {
+            BiteProbability = probability;
+        }
+
+        /// <summary>
+        /// Random chance at fish biting bait
+        /// </summary>
+        /// <returns><c>true</c>, if bait was bitten, <c>false</c> otherwise.</returns>
+        internal virtual bool BitesBait(int chance)
+        {
+            if (chance < BiteProbability)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Each fish needs to be caught using the proper technique. 
+        /// </summary>
+        /// <returns><c>true</c>, if fish was caught, <c>false</c> otherwise.</returns>
+        /// <param name="input">Input.</param>
+        internal bool IsCaught(string input)
+        {
+            if (input == "1")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Catfish : Fish
+    {
+        public Catfish(int probability) : base(probability)
+        {
+            MarketPrice = 10;
+            Name = "catfish";
+        }
+
+    }
+
+}
